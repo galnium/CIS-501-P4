9354b77fbeae7263bbb0025b733b8318ae321b51
gasadrak <gasadrak@E1114W47>
2023-10-28 16:06:21 -0500

    Added windows for server-side

---
+++ CIS-501-P4-Server/CIS-501-P4-Server/AddProduct.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CIS_501_P4_Server
+{
+    partial class AddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxProductList = new System.Windows.Forms.ListBox();
+            this.uxAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxProductList
+            // 
+            this.uxProductList.FormattingEnabled = true;
+            this.uxProductList.Location = new System.Drawing.Point(12, 12);
+            this.uxProductList.Name = "uxProductList";
+            this.uxProductList.Size = new System.Drawing.Size(196, 238);
+            this.uxProductList.TabIndex = 0;
+            // 
+            // uxAdd
+            // 
+            this.uxAdd.Location = new System.Drawing.Point(12, 256);
+            this.uxAdd.Name = "uxAdd";
+            this.uxAdd.Size = new System.Drawing.Size(196, 44);
+            this.uxAdd.TabIndex = 1;
+            this.uxAdd.Text = "Add Item";
+            this.uxAdd.UseVisualStyleBackColor = true;
+            // 
+            // AddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 309);
+            this.Controls.Add(this.uxAdd);
+            this.Controls.Add(this.uxProductList);
+            this.Name = "AddProduct";
+            this.Text = "AddProduct";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox uxProductList;
+        private System.Windows.Forms.Button uxAdd;
+    }
+}
--- CIS-501-P4-Server/CIS-501-P4-Server/Form1.Designer.cs
+++ CIS-501-P4-Server/CIS-501-P4-Server/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.uxItemList = new System.Windows.Forms.ListBox();
+            this.uxClientList = new System.Windows.Forms.ListBox();
+            this.uxAddProduct = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxItemList
+            // 
+            this.uxItemList.FormattingEnabled = true;
+            this.uxItemList.Location = new System.Drawing.Point(12, 12);
+            this.uxItemList.Name = "uxItemList";
+            this.uxItemList.Size = new System.Drawing.Size(207, 342);
+            this.uxItemList.TabIndex = 0;
+            // 
+            // uxClientList
+            // 
+            this.uxClientList.FormattingEnabled = true;
+            this.uxClientList.Location = new System.Drawing.Point(225, 12);
+            this.uxClientList.Name = "uxClientList";
+            this.uxClientList.Size = new System.Drawing.Size(207, 342);
+            this.uxClientList.TabIndex = 1;
+            // 
+            // uxAddProduct
+            // 
+            this.uxAddProduct.Location = new System.Drawing.Point(12, 360);
+            this.uxAddProduct.Name = "uxAddProduct";
+            this.uxAddProduct.Size = new System.Drawing.Size(420, 52);
+            this.uxAddProduct.TabIndex = 2;
+            this.uxAddProduct.Text = "Add Product";
+            this.uxAddProduct.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(447, 423);
+            this.Controls.Add(this.uxAddProduct);
+            this.Controls.Add(this.uxClientList);
+            this.Controls.Add(this.uxItemList);
+            this.Name = "Form1";
+            this.Text = "Server Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox uxItemList;
+        private System.Windows.Forms.ListBox uxClientList;
+        private System.Windows.Forms.Button uxAddProduct;
     }
 }
 
